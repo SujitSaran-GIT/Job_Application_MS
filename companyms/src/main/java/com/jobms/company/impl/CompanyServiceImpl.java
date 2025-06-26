aff89830ae943a19a80a699a7409a99984631b30
Student ApnaCollege <sujitsaran16@gmail.com>
2025-06-26 20:53:44 +0530

    Job Application Microservices

---
+++ companyms/src/main/java/com/jobms/company/impl/CompanyServiceImpl.java
@@ -0,0 +1,65 @@
+package com.jobms.company.impl;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jobms.company.Company;
+import com.jobms.company.CompanyRepository;
+import com.jobms.company.CompanyService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company company, Long id) {
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if (companyOptional.isPresent()){
+            Company companies = companyOptional.get();
+            companies.setName(company.getName());
+            companies.setDescription(company.getDescription());
+            
+
+            companyRepository.save(companies);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteCompany(Long id) {
+        if(companyRepository.existsById(id)){
+            companyRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Company getCompanyById(Long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+
+}
